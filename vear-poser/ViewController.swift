425b6968d67a5c6f71e44bed264f57c0f1b8fbbf
noppefoxwolf <noppelabs@gmail.com>
2020-03-30 02:07:40 +0900

    Initial Commit

---
+++ vear-poser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vear-poser
+//
+//  Created by Tomoya Hirano on 2020/03/30.
+//  Copyright © 2020 Tomoya Hirano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
